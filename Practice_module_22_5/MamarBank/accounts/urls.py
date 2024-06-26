1671840f801c6eecb80ebe9d86910a24e40e2672
Ramim Ahmed <125358464+ramim141@users.noreply.github.com>
2024-06-26 22:30:58 +0600

    Add files via upload

---
+++ Practice_module_22_5/MamarBank/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import UserLoginView, UserLogoutView, UserRegistrationView,UserUpdateView
+urlpatterns = [
+    path('register/',UserRegistrationView.as_view(),name='register'),
+    path('login/',UserLoginView.as_view(),name='login'),
+    path('logout/',UserLogoutView.as_view(),name='logout'),
+    path('profile/',UserUpdateView.as_view(),name='profile'),
+]
